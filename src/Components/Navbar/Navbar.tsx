c5a9534b3551d55337906df6159b02cb10475d36
Akele <achorisaac@gmail.com>
2023-05-21 19:44:57 +0100

    Navbar completed

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,9 +1,50 @@
 import React, { useState, ChangeEvent, FormEvent } from "react";
 import icon from "../../Images/union.png";
+import avi from "../../Images/avatar.png";
+import {
+  AiOutlineSearch,
+  AiOutlineBell,
+  AiOutlineCaretDown,
+} from "react-icons/ai";
 import iconame from "../../Images/lendsqr.png";
 import "./Navbar.scss";
 
 type Props = {};
+
+interface DropdownProps {
+  options: string[];
+}
+
+const Dropdown: React.FC<DropdownProps> = ({ options }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+
+  const handleOptionChange = (option: string) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  const handleDropdownToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="dropdown">
+      <button className="dropdown-toggle" onClick={handleDropdownToggle}>
+        <AiOutlineCaretDown className="dropdown-icon" />
+      </button>
+      {isOpen && (
+        <ul className="dropdown-menu">
+          {options.map((option) => (
+            <li key={option} onClick={() => handleOptionChange(option)}>
+              {option}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 const SearchBar: React.FC = () => {
   const [searchValue, setSearchValue] = useState("");
@@ -28,13 +69,15 @@ const SearchBar: React.FC = () => {
         className="search-input"
       />
       <button type="submit" className="search-button">
-        Search
+        <AiOutlineSearch className="button-icon" />
       </button>
     </form>
   );
 };
 
 const Navbar = (props: Props) => {
+  const dropdownOptions = ["Profile", "Loans", "Data"];
+
   return (
     <div className="top-nav">
       <div className="logo">
@@ -42,6 +85,11 @@ const Navbar = (props: Props) => {
         <img src={iconame} alt="companyname" className="navname" />
       </div>
       <SearchBar />
+      <h3 className="nav-text">Docs</h3>
+      <img src={avi} alt="" className="nav-image" />
+      <AiOutlineBell className="nav-notification" />
+      <h4 className="nav-username">Adedeji</h4>
+      <Dropdown options={dropdownOptions} />
     </div>
   );
 };
